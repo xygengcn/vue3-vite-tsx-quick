0d6b74eb98f2ca1e1c1392915f0e7a203cf87950
xygengcn <xygengcn@qq.com>
2022-05-07 21:59:17 +0800

    add: 增加eslint校验

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld";
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld';
 export default defineComponent({
-  name: "App",
+  name: 'App',
   setup() {
     return () => (
       <div class="app">
-
         这是父组件
-
         <HelloWorld></HelloWorld>
-
-       
       </div>
     );
   },
--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,16 +1,9 @@
-import { defineComponent } from "vue";
-import "./index.scss";
-const HelloWorld =defineComponent({
-  name: "App",
+import { defineComponent } from 'vue';
+import './index.scss';
+const HelloWorld = defineComponent({
+  name: 'App',
   setup() {
-    return () => (
-      <div class="app">
-
-
-        这是子组件
-       
-      </div>
-    );
+    return () => <div class="app">这是子组件</div>;
   },
 });
 export default HelloWorld;
