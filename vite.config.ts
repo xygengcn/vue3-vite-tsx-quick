11428456398d181f8f4fe91e48b9e75f11512927
xygengcn <xygengcn@qq.com>
2023-06-16 21:29:00 +0800

    update: eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-import eslint from '@rollup/plugin-eslint';
+import eslint from '@nabla/vite-plugin-eslint';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [eslint({ fix: true }), vue(), vueJsx()],
+  plugins: [eslint(), vue(), vueJsx()],
 });
