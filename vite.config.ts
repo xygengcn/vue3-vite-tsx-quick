16e64a9c45f6096b311458e47866532baecd7eef
xygengcn <xygengcn@qq.com>
2025-02-26 10:48:18 +0800

    update: 更新依赖

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,12 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 import eslint from '@nabla/vite-plugin-eslint';
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 8080,
+    https: false,
+    allowedHosts: true
+    
+  },
   plugins: [eslint(), vue(), vueJsx()],
 });
